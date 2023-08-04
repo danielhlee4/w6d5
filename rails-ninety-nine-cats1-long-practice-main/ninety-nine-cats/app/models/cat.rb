1d80c43df9b1eec18c5bf912e3146b416e3581de
Dan Lee <danielhlee4@gmail.com>
2023-08-04 16:57:15 -0400

    add #age in cat.rb

--- rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
+++ rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
@@ -1,8 +1,22 @@
-class Cat < ApplicationRecord
-    CAT_COLORS = black, white, orange, brown, grey
-    # CAT_COLORS = ("black", "white", "orange", "brown", "grey")
+require "action_view"
 
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    CAT_COLORS =["black", "white", "orange", "brown", "grey"]
 
     validates :birth_date, :color, :name, :sex, presence: true
-    validates :color, inlcusion: {in: %w(CAT_COLORS)}
+    validates :color, inclusion: { in: CAT_COLORS }
+    validates :sex, inclusion: { in: %w(M F)}
+    validate :birth_date_cannot_be_future
+
+    def birth_date_cannot_be_future
+        if birth_date.present? && self.birth_date > Date.today
+            errors.add(:birth_date, "cannot be in the future")
+        end
+    end
+
+    def age
+
+        time_ago_in_words(self.birth_date)
+    end
 end
